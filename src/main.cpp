d1ed4291c81f286560abfc2e06db670b96550d85
localrice <borokinjalkavish@gmail.com>
2025-06-24 10:45:05 +0530

    add: control oled screen using json via the serial input

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,72 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <ArduinoJson.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_RESET -1
+#define SCREEN_ADDRESS 0x3C
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+String inputBuffer = "";
+
+void processJson(String jsonStr) {
+  StaticJsonDocument<256> doc;
+  DeserializationError error = deserializeJson(doc, jsonStr);
+
+  if (error) {
+    Serial.print(F("JSON Parse Error: "));
+    Serial.println(error.c_str());
+    return;
+  }
+
+  if (doc.containsKey("size")) {
+    display.setTextSize(doc["size"]);
+  }
+
+  if (doc.containsKey("pos") && doc["pos"].is<JsonArray>()) {
+    int x = doc["pos"][0];
+    int y = doc["pos"][1];
+    display.setCursor(x, y);
+  }
+
+  if (doc.containsKey("text")) {
+    display.clearDisplay();
+    display.setTextColor(SSD1306_WHITE);
+    display.println(doc["text"].as<String>());
+    display.display();
+  }
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Wire.begin(D2, D1);
+  Serial.begin(115200);
+  while (!Serial) delay(10);
+
+  if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 init failed"));
+    while (true);
+  }
+
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 0);
+  display.println("JSON OLED Ready");
+  display.display();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  while (Serial.available()) {
+    char ch = Serial.read();
+    if (ch == '\n') {
+      processJson(inputBuffer);
+      inputBuffer = "";
+    } else {
+      inputBuffer += ch;
+    }
+  }
 }
